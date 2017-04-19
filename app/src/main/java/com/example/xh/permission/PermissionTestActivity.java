565570ab122ec447059af7296349404429a2eccf
xiehui <xiehui0902@gmail.com>
2017-04-19 08:49:30 +0800

    Request permission  about WRITE_SETTINGS and SYSTEM_ALERT_WINDOW
    Request permission  about WRITE_SETTINGS and SYSTEM_ALERT_WINDOW

--- app/src/main/java/com/example/xh/permission/PermissionTestActivity.java
+++ app/src/main/java/com/example/xh/permission/PermissionTestActivity.java
@@ -1,9 +1,14 @@
 package com.example.xh.permission;
 
 import android.Manifest;
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -25,6 +30,8 @@ public class PermissionTestActivity extends AppCompatActivity {
     TextView tv_deviceId;
     private static final String[] permissions = new String[]{Manifest.permission.READ_PHONE_STATE};
     private static final int requestCode = 0x0001;
+    private static final int requestCodeWriteSetting = 0x0002;
+    private static final int requestCodeAlertWindow = 0x0003;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -33,6 +40,9 @@ public class PermissionTestActivity extends AppCompatActivity {
         ButterKnife.bind(this);
     }
 
+    /**
+     * 测试请求读取手机卡权限
+     */
     @OnClick(R.id.request)
     public void requestDeviceId() {
         if (PermissionUtils.isHasPermissions(permissions)) {
@@ -43,10 +53,83 @@ public class PermissionTestActivity extends AppCompatActivity {
 
     }
 
+    /**
+     * 测试请求WRITE_SETTINGS权限
+     */
+    @OnClick(R.id.request_write_setting)
+    @TargetApi(android.os.Build.VERSION_CODES.M)
+    public void requestWriteSetting() {
+        if (!Settings.System.canWrite(this)) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS,
+                    Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, requestCodeWriteSetting);
+        } else {
+            Toast.makeText(PermissionTestActivity.this, "WRITE_SETTINGS 已经被授权", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * 测试请求SYSTEM_ALERT_WINDOW权限
+     */
+    @OnClick(R.id.request_alert_window)
+    @TargetApi(android.os.Build.VERSION_CODES.M)
+    public void requestAlertWindow() {
+        if (!Settings.canDrawOverlays(this)) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, requestCodeAlertWindow);
+        } else {
+            Toast.makeText(PermissionTestActivity.this, "SYSTEM_ALERT_WINDOW 已经被授权", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * 打开应用权限设置界面
+     */
+    @OnClick(R.id.open_permission_setting)
+    public void requestOpenPermissionSetting() {
+        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        // Uri uri = Uri.fromParts("package", getPackageName(), null);
+        Uri uri1=Uri.parse("package:" + getPackageName());
+        intent.setData(uri1);
+        startActivity(intent);
+    }
+
     private void executeReadDeviceId() {
         TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
         String deviceId = telephonyManager.getDeviceId();
         tv_deviceId.setText("DeviceId:" + deviceId);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == requestCodeWriteSetting) {
+            showToast();
+        } else if (requestCode == requestCodeAlertWindow) {
+            showToastAlerterWindow();
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private void showToast() {
+        if (Settings.System.canWrite(this)) {
+            //检查返回结果
+            Toast.makeText(PermissionTestActivity.this, "WRITE_SETTINGS 被授权", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(PermissionTestActivity.this, "WRITE_SETTINGS 没有被授权", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private void showToastAlerterWindow() {
+        if (Settings.System.canWrite(this)) {
+            //检查返回结果
+            Toast.makeText(PermissionTestActivity.this, "SYSTEM_ALERT_WINDOW 被授权", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(PermissionTestActivity.this, "SYSTEM_ALERT_WINDOW 没有被授权", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
